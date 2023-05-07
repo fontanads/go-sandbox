f07fa985bff50ea9721c3f6084b99872d7664248
fontanads <fontanads@gmail.com>
2023-05-08 00:23:02 +0100

    new toy examples

---
+++ sandbox/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("2 * 9 = ", 2*9)
+	fmt.Println("9.0 / 2 = ", 9.0/2)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
